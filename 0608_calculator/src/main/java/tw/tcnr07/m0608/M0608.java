651b1336905c9bc8e7fc75fdae60215182fb00cf
Lin yi ping <95246311+yiping560@users.noreply.github.com>
2021-11-30 16:32:18 +0800

    1101130-2

---
+++ 0608_calculator/src/main/java/tw/tcnr07/m0608/M0608.java
@@ -0,0 +1,174 @@
+package tw.tcnr07.m0608;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+public class M0608 extends AppCompatActivity {
+
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10;
+    private Button btn11, btn12, btn13, btn14, btn15;
+    private TextView num1;
+    private TextView ans;
+    private String s;
+    private double nn;
+    private double n2;
+    private DecimalFormat nf;
+    private double n1;
+    private String keyin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.m0608);
+        setupViewCompoent();
+    }
+
+    private void setupViewCompoent() {
+        num1=(TextView)findViewById(R.id.m0608_e001);
+        num1.setText("");
+        ans = (TextView) findViewById(R.id.m0608_t001);
+        btn0 = (Button) findViewById(R.id.m0608_b000);
+        btn1 = (Button) findViewById(R.id.m0608_b001);
+        btn2 = (Button) findViewById(R.id.m0608_b002);
+        btn3 = (Button) findViewById(R.id.m0608_b003);
+        btn4 = (Button) findViewById(R.id.m0608_b004);
+        btn5 = (Button) findViewById(R.id.m0608_b005);
+        btn6 = (Button) findViewById(R.id.m0608_b006);
+        btn7 = (Button) findViewById(R.id.m0608_b007);
+        btn8 = (Button) findViewById(R.id.m0608_b008);
+        btn9 = (Button) findViewById(R.id.m0608_b009);
+        btn10 = (Button) findViewById(R.id.m0608_b010);//.
+
+        btn11 = (Button) findViewById(R.id.m0608_b011);// +
+        btn12 = (Button) findViewById(R.id.m0608_b012);// -
+        btn13 = (Button) findViewById(R.id.m0608_b013);// *
+        btn14 = (Button) findViewById(R.id.m0608_b014);// /
+        btn15 = (Button) findViewById(R.id.m0608_b015); // =
+
+        btn0.setOnClickListener(numberOnClick);
+        btn1.setOnClickListener(numberOnClick);
+        btn2.setOnClickListener(numberOnClick);
+        btn3.setOnClickListener(numberOnClick);
+        btn4.setOnClickListener(numberOnClick);
+        btn5.setOnClickListener(numberOnClick);
+        btn6.setOnClickListener(numberOnClick);
+        btn7.setOnClickListener(numberOnClick);
+        btn8.setOnClickListener(numberOnClick);
+        btn9.setOnClickListener(numberOnClick);
+        btn10.setOnClickListener(numberOnClick);
+
+        btn11.setOnClickListener(calOnClick);
+        btn12.setOnClickListener(calOnClick);
+        btn13.setOnClickListener(calOnClick);
+        btn14.setOnClickListener(calOnClick);
+
+        btn15.setOnClickListener(equalOnClick);
+    }
+
+
+    private View.OnClickListener numberOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            keyin=num1.getText().toString();
+
+            switch (v.getId()) {
+                case R.id.m0608_b000:
+                        num1.setText(keyin+"0");
+                    break;
+                case R.id.m0608_b001:
+                        num1.setText(keyin+"1");
+                    break;
+                case R.id.m0608_b002:
+                        num1.setText(keyin+"2");
+                    break;
+                case R.id.m0608_b003:
+                        num1.setText(keyin+"3");
+                    break;
+                case R.id.m0608_b004:
+                        num1.setText(keyin+"4");
+                    break;
+                case R.id.m0608_b005:
+                    num1.setText(keyin+"5");
+                    break;
+                case R.id.m0608_b006:
+                    num1.setText(keyin+"6");
+                    break;
+                case R.id.m0608_b007:
+                    num1.setText(keyin+"7");
+                    break;
+                case R.id.m0608_b008:
+                    num1.setText(keyin+"8");
+                    break;
+                case R.id.m0608_b009:
+                    num1.setText(keyin+"9");
+                    break;
+                case R.id.m0608_b010:
+                    num1.setText(keyin+".");
+                    break;
+            }
+        }
+    };
+
+
+    private View.OnClickListener calOnClick=new View.OnClickListener() {
+
+
+        @Override
+        public void onClick(View v) {
+            try {
+                nn=Double.parseDouble(num1.getText().toString());
+            }catch (Exception e){
+                Toast.makeText(getApplicationContext(), getString(R.string.error), Toast.LENGTH_LONG).show();
+            }
+
+            num1.setText("");
+            switch (v.getId()){
+                case R.id.m0608_b011:
+                    s="P";
+                    n1=n1+nn;
+                    break;
+                case R.id.m0608_b012:
+                    s="M";
+                    n1=n1 -nn;
+                    break;
+                case R.id.m0608_b013:
+                    s="X";
+                    n1=n1 * nn;
+                    break;
+                case R.id.m0608_b014:
+                    s="D";
+                    n1=n1 / nn;
+                    break;
+            }
+        }
+    };
+
+
+
+    private View.OnClickListener equalOnClick=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+        if(num1.getText().toString().trim().length()!=0){
+                n2=Double.parseDouble(num1.getText().toString());
+                nf=new DecimalFormat("0.0000");
+                if(s.equals("P")) ans.setText(nf.format(nn+n2));
+                if(s.equals("M")) ans.setText(nf.format(nn-n2));
+                if(s.equals("X")) ans.setText(nf.format(nn*n2));
+                if(s.equals("D")) ans.setText(nf.format(nn/n2));
+
+            num1.setText("");
+            nn=0;
+
+        }else{
+            Toast.makeText(getApplicationContext(), getString(R.string.m0608_error), Toast.LENGTH_LONG).show();
+        }
+        }
+    };
+}
